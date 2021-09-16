f4158749d41d06b951158292202e928361c78cb6
bkins <benhop@gmail.com>
2021-09-15 22:11:02 -0700

    Add project files.

---
+++ InsAndOuts/InsAndOuts/Views/NewItemPage.xaml.cs
@@ -0,0 +1,21 @@
+﻿using InsAndOuts.Models;
+using InsAndOuts.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace InsAndOuts.Views
+{
+    public partial class NewItemPage : ContentPage
+    {
+        public Item Item { get; set; }
+
+        public NewItemPage()
+        {
+            InitializeComponent();
+            BindingContext = new NewItemViewModel();
+        }
+    }
+}
